cef38fdc65cf3599cd30b319f661e2e739b47b1b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-06-18 22:42:02 -0300

    feat: change header color and add animation

--- scripts/_script.js
+++ scripts/_script.js
@@ -5,3 +5,7 @@ menuButton.onclick = () => toggleMobileMenu();
 // Header links and background animation
 import { styleActiveLink } from "./styleActiveLink.js";
 styleActiveLink();
+
+// Toggle header visibility when scrolling
+import { toggleHeaderVisibility } from "./toggleHeaderVisibility.js";
+toggleHeaderVisibility();
